52507fcaf0ed95eb1ab57ee21ef0bdd308e6c573
Carmine Laudato <c.laudato2@studenti.unisa.it>
2021-01-18 09:04:00 +0100

    Update
    Co-Authored-By: grapha99 <74360088+grapha99@users.noreply.github.com>

--- src/view/js/script.js
+++ src/view/js/script.js
@@ -5,8 +5,38 @@ function upgradeMiPiace(){
     document.getElementById("voti").innerHTML = contatore
 }
 
-function returnRating(){
-    const container= document.querySelector('.star-rating');
+function returnRatingGP(){
+    const container= document.querySelector('.star-ratingGP');
+    const items=container.querySelectorAll('.rating')
+
+    container.onclick= e =>{
+        const elClass=e.target.classList;
+        console.log(e.target.getAttribute("value"));
+    }
+}
+
+function returnRatingT(){
+    const container= document.querySelector('.star-ratingT');
+    const items=container.querySelectorAll('.rating')
+
+    container.onclick= e =>{
+        const elClass=e.target.classList;
+        console.log(e.target.getAttribute("value"));
+    }
+}
+
+function returnRatingG(){
+    const container= document.querySelector('.star-ratingG');
+    const items=container.querySelectorAll('.rating')
+
+    container.onclick= e =>{
+        const elClass=e.target.classList;
+        console.log(e.target.getAttribute("value"));
+    }
+}
+
+function returnRatingVC(){
+    const container= document.querySelector('.star-ratingVC');
     const items=container.querySelectorAll('.rating')
 
     container.onclick= e =>{
