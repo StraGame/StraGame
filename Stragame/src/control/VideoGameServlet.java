b8aab54a3316d8132a3ea4c1d31152a9b988ce8e
Carmine Laudato <c.laudato2@studenti.unisa.it>
2021-01-22 07:34:54 +0100

    Update Control
    Co-Authored-By: grapha99 <74360088+grapha99@users.noreply.github.com>

---
+++ Stragame/src/control/VideoGameServlet.java
@@ -0,0 +1,83 @@
+package control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.VideoGameBean;
+import model.VideoGameDao;
+import model.VideoGameDto;
+
+/**
+ * Servlet implementation class VideoGameServelet
+ */
+@WebServlet("/VideoGameServelet")
+public class VideoGameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private VideoGameDao videogamedto=new VideoGameDto();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public VideoGameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String label="";
+		String titolo= request.getParameter("titolo");
+		String genere= request.getParameter("genere");
+		String descrizione=request.getParameter("descrizione");
+		
+		try {
+			if((!"".equals(titolo)) &&(!"".equals(descrizione))) {
+					if(videogamedto.getVideoGame(titolo).getNome()=="") {
+						
+						VideoGameBean bean= new VideoGameBean();
+						
+						bean.setDescrizione(descrizione);
+						bean.setNome(titolo);
+						bean.setGenere(genere);
+						
+						videogamedto.insertVideoGame(bean);
+						label="Videogioco inserito con successo";
+						
+					} 
+					else {
+						label="Videogioco già presente";
+					}
+			} 
+			else {
+				label="Inserisci tutti i campi";
+			}
+			request.setAttribute("label", label);
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/newGame.jsp");
+			dispatcher.forward(request, response);
+			} 
+		catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+}
