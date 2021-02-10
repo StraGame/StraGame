7b25b30e3872b2b13f7614800ae8426729c9540d
Carmine Laudato <c.laudato2@studenti.unisa.it>
2021-02-10 16:21:22 +0100

    update

--- StraGame_NC13/src/main/java/model/DriverManagerConnectionPool.java
+++ StraGame_NC13/src/main/java/model/DriverManagerConnectionPool.java
@@ -25,7 +25,7 @@ public class DriverManagerConnectionPool {
     String port = "3306";
     String db = "stragame";
     String username = "root";
-    String password = "amilo33";
+    String password = "root";
 
     newConnection = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + db 
         + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", username, password);
